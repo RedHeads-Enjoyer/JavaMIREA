fee2b9a6efc969c818cce6c1f9289e8b22bf2c0c
LASOV <44479103+Weestle@users.noreply.github.com>
2022-09-19 23:15:27 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        System.out.println("Ex 1");
-//        InsertionTest insertionTest = new InsertionTest();
-//        insertionTest.test();
-//
-//        System.out.println("\n\nEx 2");
-//        QuickTest quickTest = new QuickTest();
-//        quickTest.test();
+        System.out.println("Ex 1");
+        InsertionTest insertionTest = new InsertionTest();
+        insertionTest.test();
+
+        System.out.println("\n\nEx 2");
+        QuickTest quickTest = new QuickTest();
+        quickTest.test();
 
         System.out.println("\n\nEx 3");
         MergeTest mergeTest = new MergeTest();
