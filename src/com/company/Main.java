719aa475d475c40069b23fce8e13a5328e9c714e
LASOV <44479103+Weestle@users.noreply.github.com>
2022-10-02 17:55:31 +0300

    11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        System.out.println("\nEx1");
-//        new Ex1().test();
-//
-//        System.out.println("\n\nEx2");
-//        new Ex2().test();
-//
-//        System.out.println("\nEx3");
-//        new Ex3().test();
-//
-//        System.out.println("\nEx4");
-//        new Ex4().test();
+        System.out.println("\nEx1");
+        new Ex1().test();
+
+        System.out.println("\n\nEx2");
+        new Ex2().test();
+
+        System.out.println("\nEx3");
+        new Ex3().test();
+
+        System.out.println("\nEx4");
+        new Ex4().test();
 
         System.out.println("\nEx5");
         new Ex5().test();
