c75004616dfcc4d4a4d48cbde04c47b89ab8687c
LASOV <44479103+Weestle@users.noreply.github.com>
2022-10-05 20:02:49 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static  int rec1(int a, int b) {
+    public static boolean isFull = false;
+
+    public static  int rec1(int a, int b, int i) {
+        i++;
+        if (i > 100) {
+            isFull = true;
+        }
+        if (isFull) {
+            return -1;
+        }
         if (a > b + 1) return 0;
         if (a == 0 || b == 0) return 1;
-        return rec1(a - 1, b - 1) + rec1(a, b - 1);
+        return rec1(a - 1, b - 1, i) + rec1(a, b - 1, i);
     }
 
     public static int rec2(int n, int a) {
@@ -28,11 +37,12 @@ public class Main {
         }
     }
 
-    public static void rec4() {
+    public static void rec4(String s) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        if (a != 0) rec4();
-        if (a % 2 == 0 && a != 0) System.out.println(a);
+        if (a % 2 == 1) s += a + " ";
+        if (a != 0) rec4(s);
+        else System.out.println(s);
     }
 
     public static void main(String[] args) {
@@ -43,19 +53,22 @@ public class Main {
         System.out.println("Ex 9");
         int a = scanner.nextInt(); // 0
         int b = scanner.nextInt(); // 1
-        System.out.println(rec1(a, b));
+        int l = rec1(a, b, 0);
+        if (!isFull) {
+            System.out.println(l);
+        }
+        else System.out.println("Рекурсия переполнена!");
 
         System.out.println("Ex 10");
         int n = scanner.nextInt();
-        System.out.println(rec2(1234, 0));
+        System.out.println(rec2(n, 0));
 
         System.out.println("Ex 11");
         int s = rec3(2, 0);
         System.out.println(s);
 
         System.out.println("Ex 12");
-        rec4();
-
+        rec4("");
     }
 }
 
